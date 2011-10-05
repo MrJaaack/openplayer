eefa063aa74a9e6bf74a2fef05c010392e998612
bbrodriges <bender@rodriges.org>
2011-10-05 16:58:19 +0400

    Additional function to return exact same song when user shares url

--- lib/AudioParser.php
+++ lib/AudioParser.php
@@ -85,6 +85,16 @@ class AudioParser {
             if (preg_match('#<span class="title">(.+?)<span class="user">#', $s, $name)) {
             	$song['name'] = strip_tags(trim($name[1]));
             }
+			
+			preg_match_all( // »щет id вида 41613828_110901414
+                '/<div class="repeat_wrap">(.*)?<\/div>/',
+                $audioItem,
+                $res2
+            );
+			
+			if (preg_match('#id="repeat(.+?)"#', $res2[1][0], $play_id)) { // «аписывает id вида 41613828_110901414
+				$song['vkid'] = $play_id[1];
+            }
 
 			if ( $config->getOption('app', 'fair_id') == 'yes' ) {
 				$headers = \Lib\Curl::get_headers($song['url'], true);
@@ -113,5 +123,93 @@ class AudioParser {
 
         return $songs;
     }
+	
+	/* ѕринимает парамерт id вида 41613828_110901414 (пользователь_песн€). ƒанный параметр содержитс€ в переменной songs[song[id]][vkid] */
+	/* ћожно генерировать ссылки виды http://yoursite.ru/?song=41613828_110901414 и гарантировано возращать ту самую песню */
+	public static function searchByID($Linkid) {
+        $config = Config::getInstance();
+        $cookie = VkLogin::getCookie();
+		
+		$result = '';
+
+        $ids = $config->getOption('vk', 'id');
+		
+		$AudioIDs = explode('_', $Linkid);
+
+        $post = array(
+            'act' => 'load_audios_silent',
+            'al' => '1',
+            'edit' => '0',
+			'gid' => '0',
+            'id' => $AudioIDs[0]
+        );
+
+        $answer = Curl::process(
+            'http://vkontakte.ru/audio',
+            $cookie,
+            false,
+            http_build_query($post)
+        );
+		
+		$pos = strpos($answer, '{');
+		$answer = substr($answer, $pos+8);
+		$tmp = explode('<!>', $answer);
+		$answer = substr($tmp[0], 0, strlen($tmp[0])-2);
+		$answer = str_replace('"', '&quote;', $answer);
+		$answer = str_replace('\',\'', '","', $answer);
+		$answer = str_replace('[\'', '["', $answer);
+		$answer = str_replace('\']', '"]', $answer);
+		$answer = '{"all":['.self::win2utf($answer).']}';
+		$answer = json_decode($answer, TRUE);
+		
+		foreach($answer['all'] as $element) {
+			if(array_search($AudioIDs[1], $element)) {
+				$result = $element;
+				break;
+			}
+		}
+		if($result) {
+			
+			$song['url'] = $result[2];
+			$song['duration'] = $result[4];
+			$song['artist'] = $result[5];
+			$song['name'] = $result[6];
+			$song['translate'] = '0';
+			
+			return $song; // ¬озвращает массив с одной песней. ƒанна€ песн€ - 100% именно та, на которую дают ссылку
+		} else {
+			return array();
+		}
+    }
+	
+	function win2utf($str)	{ // ѕереводит win-1251 в utf8, так как  онтакт на запрос act=load_audios_silent отвечает в этой древней кодировке
+		static $table = array(
+		"\xA8" => "\xD0\x81",
+		"\xB8" => "\xD1\x91",
+		"\xA1" => "\xD0\x8E",
+		"\xA2" => "\xD1\x9E",
+		"\xAA" => "\xD0\x84",
+		"\xAF" => "\xD0\x87",
+		"\xB2" => "\xD0\x86",
+		"\xB3" => "\xD1\x96",
+		"\xBA" => "\xD1\x94",
+		"\xBF" => "\xD1\x97",
+		"\x8C" => "\xD3\x90",
+		"\x8D" => "\xD3\x96",
+		"\x8E" => "\xD2\xAA",
+		"\x8F" => "\xD3\xB2",
+		"\x9C" => "\xD3\x91",
+		"\x9D" => "\xD3\x97",
+		"\x9E" => "\xD2\xAB",
+		"\x9F" => "\xD3\xB3",
+		);
+		return preg_replace('#[\x80-\xFF]#se',
+		' "$0" >= "\xF0" ? "\xD1".chr(ord("$0")-0x70) :
+						   ("$0" >= "\xC0" ? "\xD0".chr(ord("$0")-0x30) :
+							(isset($table["$0"]) ? $table["$0"] : "")
+						   )',
+		$str
+		);
+	}
 
 }
