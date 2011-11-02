d1cee333a92b3e698311132cba1a2beb5b427cf0
Artem Bondarenko <artbonvic@gmail.com>
2011-11-02 12:55:44 +0200

    Update lib/Curl.php

--- lib/Curl.php
+++ lib/Curl.php
@@ -4,55 +4,55 @@ namespace Lib;
 class Curl {
 
     public static function process( $url, $cookie=false, $headers=false, $post=false ) {
-        $ch = curl_init($url);
+        $ch = \curl_init($url);
         
-        curl_setopt(
+        \curl_setopt(
             $ch, 
             CURLOPT_RETURNTRANSFER, 
             true
         );
         
-        curl_setopt(
+        \curl_setopt(
             $ch, 
             CURLOPT_HEADER, 
             $headers
         );
         
-        curl_setopt(
+        \curl_setopt(
             $ch, 
             CURLOPT_FOLLOWLOCATION, 
             0
         );
         
-        curl_setopt(
+        \curl_setopt(
             $ch, 
             CURLOPT_COOKIE, 
             $cookie
         );
         
-        curl_setopt(
+        \curl_setopt(
             $ch, 
             CURLOPT_USERAGENT, 
             Config::getInstance()->getOption('vk', 'userAgent')
         );
         
         if ($post) {
-            curl_setopt(
+            \curl_setopt(
                 $ch, 
                 CURLOPT_POST, 
                 1
             );
             
-            curl_setopt(
+            \curl_setopt(
                 $ch, 
                 CURLOPT_POSTFIELDS, 
                 $post
             );
         }
         
-        $response = curl_exec($ch);
+        $response = \curl_exec($ch);
         
-        curl_close($ch);
+        \curl_close($ch);
         
         return $response;
     }
