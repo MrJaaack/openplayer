b527072e17c6447aaf07d27161d6ac069e6d411a
Artem Bondarenko <ar4ibal@gmail.com>
2011-11-30 20:47:51 +0200

    Player update

--- app/Index.php
+++ app/Index.php
@@ -7,6 +7,7 @@ class Index extends \Lib\Base\App {
     public function init() {
         $q = \Lib\Request::get('q');
         if ( $q ) {
+            $this->title = "Слушать {$q} онлайн, Скачать {$q}";
         }
     }
     
